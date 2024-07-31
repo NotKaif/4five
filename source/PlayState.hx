419e55007e3feb11d8e24a526f6dc131554ba8ad
Not_Kaif <notkaif.p@gmail.com>
2024-07-31 14:13:23 +0100

    Initial commit

---
+++ source/PlayState.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		var sprite:FlxSprite = new FlxSprite(10, 10);
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
